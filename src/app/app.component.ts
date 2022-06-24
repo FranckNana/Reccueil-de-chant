e09b8da9e6ec3b80b9d89eef4870bbdf448528d8
NANAFRANCK <franjudina@gmail.com>
2022-06-25 01:32:21 +0200

    modification programme ordi en menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,14 @@ export class AppComponent {
   title = 'song-app';
 
   constructor(){
-    var firebaseConfig = {
-      apiKey: "AIzaSyAjutuuUPAsaJh_7HBd0HVQ_6SHeclNTCw",
-      authDomain: "carnet-de-chants-130f7.firebaseapp.com",
-      databaseURL: "https://carnet-de-chants-130f7-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "carnet-de-chants-130f7",
-      storageBucket: "carnet-de-chants-130f7.appspot.com",
-      messagingSenderId: "251921900124",
-      appId: "1:251921900124:web:98587cb22c193f6fedfc99"
+    const firebaseConfig = {
+      apiKey: "AIzaSyAoFNQmSaM9M01OLyPCXOtkP8NVGw7K7r0",
+      authDomain: "song-app-admin.firebaseapp.com",
+      projectId: "song-app-admin",
+      storageBucket: "song-app-admin.appspot.com",
+      messagingSenderId: "437253215814",
+      appId: "1:437253215814:web:00cb93477abc873788dc47",
+      measurementId: "G-PK0PSXB0VE"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
