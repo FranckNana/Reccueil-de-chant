2948b41bad73fff7d66ecc45d927df61f9f1622d
FranckNana <58057749+FranckNana@users.noreply.github.com>
2022-07-12 03:16:32 +0200

    Update file.service.ts

--- src/app/services/fileService/file.service.ts
+++ src/app/services/fileService/file.service.ts
@@ -161,9 +161,9 @@ export class FileService {
   uploadFile(file: File, isPartition: boolean) {
     return new Promise(
       (resolve, reject) => {
-        const almostUniqueFileName = Date.now().toString();
+        const almostUniqueFileName = Date.now().toString()+"/";
         if(isPartition){
-          const upload = firebase.storage().ref('files/partitions').child(almostUniqueFileName+"/"+file.name).put(file);
+          const upload = firebase.storage().ref('files/partitions').child(almostUniqueFileName+file.name).put(file);
           upload.on(firebase.storage.TaskEvent.STATE_CHANGED,
             () => {
               console.log('Chargement…');
@@ -179,7 +179,7 @@ export class FileService {
             }
           );
         }else{
-          const upload = firebase.storage().ref('files/programmes').child(almostUniqueFileName+"/"+file.name).put(file);
+          const upload = firebase.storage().ref('files/programmes').child(almostUniqueFileName+file.name).put(file);
           upload.on(firebase.storage.TaskEvent.STATE_CHANGED,
             () => {
               console.log('Chargement…');
